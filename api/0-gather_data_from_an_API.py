918b42aea5e93c8c83b26231d138ffd7f275f5c8
shania178 <b.tinkouree@alustudent.com>
2026-08-20 16:13:53 +0000

    Fixing PEP8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,53 +1,45 @@
 #!/usr/bin/python3
-
 """Module that gathers data from an API for a given employee ID.
 
-
-Using the JSONPlaceholder REST API, this script fetches an employee's TODO list and displays their progress on the standard output.
+Fetches an employee's TODO list from the JSONPlaceholder REST API
+and displays their task completion progress on the standard output.
 """
+import sys
 
 import requests
 
 
-import sys
-
-
 if __name__ == "__main__":
-	try:
-		employee_id = int(sys.argv[1])
-	except (IndexError, ValueError):
-		print("Please provide a valid integer employer ID")
-	else:
-		try:
-			base_url = "https://jsonplaceholder.typicode.com"
+    try:
+        employee_id = int(sys.argv[1])
+    except (IndexError, ValueError):
+        print("Please provide a valid integer employee ID")
+    else:
+        try:
+            base_url = "https://jsonplaceholder.typicode.com"
 
+            user_response = requests.get(
+                "{}/users/{}".format(base_url, employee_id))
+            user_data = user_response.json()
+            employee_name = user_data.get("name")
 
-			user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-			user_data = user_response.json()
-			employee_name = user_data.get("name")
+            todos_response = requests.get(
+                "{}/todos?userId={}".format(base_url, employee_id))
+            todos_data = todos_response.json()
 
+            total_tasks = len(todos_data)
+            completed_tasks = []
+            for task in todos_data:
+                if task.get("completed"):
+                    completed_tasks.append(task)
 
-			todos_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
+            num_completed_tasks = len(completed_tasks)
 
+            print("Employee {} is done with tasks({}/{}):".format(
+                employee_name, num_completed_tasks, total_tasks))
 
-			todos_data = todos_response.json()
+            for task in completed_tasks:
+                print("\t {}".format(task.get("title")))
 
-			total_tasks = len(todos_data)
-			completed_tasks = []
-			for task in todos_data:
-				if task.get("completed"):
-					completed_tasks.append(task)
-
-
-			num_completed_tasks = len(completed_tasks)
-
-
-			print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
-
-
-			for task in completed_tasks:
-				print("\t {}".format(task.get("title")))
-
-	
-		except Exception as e:
-			print("An error occured: {}".format(e))
+        except Exception as e:
+            print("An error occurred: {}".format(e))
